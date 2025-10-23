c7c357e8771f1fc3d0a60710a8a2dc058268bb6a
Daniel Láštic <daniellastic@gmail.com>
2025-10-23 11:34:14 +0200

    Add clear data button

--- src/App.jsx
+++ src/App.jsx
@@ -8,47 +8,46 @@ import LanguagesList from "./components/LanguagesList";
 import CVDisplay from "./components/CVDisplay";
 import exampleData from "./data/exampleData";
 
+const initializePersonalInfo = () => ({
+  name: "",
+  email: "",
+  phone: "",
+  address: "",
+  linkedin: "",
+  github: "",
+});
+
+const initializeExperience = () => ({
+  id: Date.now(),
+  company: "",
+  jobTitle: "",
+  startDate: "",
+  endDate: "",
+  description: "",
+});
+
+const initializeEducation = () => ({
+  id: Date.now(),
+  institution: "",
+  degree: "",
+  startDate: "",
+  endDate: "",
+});
+
+const initializeSkill = () => ({ id: Date.now(), name: "" });
+
+const initializeLanguage = () => ({
+  id: Date.now(),
+  lang: "",
+  proficiency: "",
+});
+
 export default function App() {
-  const [personalInfo, setPersonalInfo] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    address: "",
-    linkedin: "",
-    github: "",
-  });
-  const [experiences, setExperiences] = useState([
-    {
-      id: Date.now(),
-      company: "",
-      jobTitle: "",
-      startDate: "",
-      endDate: "",
-      description: "",
-    },
-  ]);
-  const [educations, setEducations] = useState([
-    {
-      id: Date.now(),
-      institution: "",
-      degree: "",
-      startDate: "",
-      endDate: "",
-    },
-  ]);
-  const [skills, setSkills] = useState([
-    {
-      id: Date.now(),
-      name: "",
-    },
-  ]);
-  const [languages, setLanguages] = useState([
-    {
-      id: Date.now(),
-      lang: "",
-      proficiency: "",
-    },
-  ]);
+  const [personalInfo, setPersonalInfo] = useState(initializePersonalInfo());
+  const [experiences, setExperiences] = useState([initializeExperience()]);
+  const [educations, setEducations] = useState([initializeEducation()]);
+  const [skills, setSkills] = useState([initializeSkill()]);
+  const [languages, setLanguages] = useState([initializeLanguage()]);
 
   function loadExample() {
     setPersonalInfo(exampleData.personalInfo);
@@ -58,10 +57,19 @@ export default function App() {
     setLanguages(exampleData.languages);
   }
 
+  function clearData() {
+    setPersonalInfo(initializePersonalInfo());
+    setExperiences([initializeExperience()]);
+    setEducations([initializeEducation()]);
+    setSkills([initializeSkill()]);
+    setLanguages([initializeLanguage()]);
+  }
+
   return (
     <div className="App">
       <div className="edit-section">
         <button onClick={loadExample}>Load Example</button>
+        <button onClick={clearData}>Clear Data</button>
         <h1>CV Generator</h1>
         <PersonalInfo info={personalInfo} setInfo={setPersonalInfo} />
         <ExperienceList
